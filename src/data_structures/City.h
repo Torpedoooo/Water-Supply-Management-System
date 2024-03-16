de5d3ab20dfc2dedf14a34d17f7bc9489c0a4d9b
Rui Borges <ruiborges206@gmail.com>
2024-03-16 11:21:15 +0000

    Cities.h

--- src/data_structures/City.h
+++ src/data_structures/City.h
@@ -8,6 +8,54 @@
 #include <string>
 
 class City {
+
+private:
+    int id;
+    int demand;
+    int population;
+    std::string City;
+    std::string Code;
+
+public:
+    int getId() const {
+        return id;
+    }
+
+    void setId(int id) {
+        City::id = id;
+    }
+
+    int getDemand() const {
+        return demand;
+    }
+
+    void setDemand(int demand) {
+        City::demand = demand;
+    }
+
+    int getPopulation() const {
+        return population;
+    }
+
+    void setPopulation(int population) {
+        City::population = population;
+    }
+
+    const std::string &getCity() const {
+        return City;
+    }
+
+    void setCity(const std::string &city) {
+        City = city;
+    }
+
+    const std::string &getCode() const {
+        return Code;
+    }
+
+    void setCode(const std::string &code) {
+        Code = code;
+    }
 };
 
 
